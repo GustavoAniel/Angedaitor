bcd094e118ba940ec25d94635a7a677a0137e5be
Gustavo <aniel.amtimil@yahoo.com.br>
2022-05-04 00:06:33 -0300

    Criado um Model para o Lembrete

---
+++ src/model/Lembrete.js
@@ -0,0 +1,8 @@
+export default class Lembrete {
+    constructor(titulo, inicio, final, descricao){
+        this.titulo = titulo;
+        this.inicio = inicio;
+        this.final = final;
+        this.descricao = descricao;
+    }
+}
