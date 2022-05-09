031715649279869a2439c45a170005604df3b6e6
Gustavo <aniel.amtimil@yahoo.com.br>
2022-05-09 13:28:14 -0300

    Estilo do Modal

---
+++ src/styles/Modal.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+const styleModal = StyleSheet.create({
+    titulo: {
+        fontWeight: 'bold',
+        fontSize: 20
+    },
+    texto: {
+        fontSize: 15,
+        marginVertical: 5
+    }
+})
+
+export default styleModal;
