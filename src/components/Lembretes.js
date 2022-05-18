aff766a05b69cc45f2f8a5a58ab5e90c165ec80e
Gustavo <aniel.amtimil@yahoo.com.br>
2022-05-18 09:57:06 -0300

    Horario de lembretes com o formato certo

--- src/components/Lembretes.js
+++ src/components/Lembretes.js
@@ -5,16 +5,31 @@ import styleLemb from '../styles/Lembrete'
 import IconIO from 'react-native-vector-icons/Ionicons'
 import IconFeather from 'react-native-vector-icons/Feather'
 
-export default function Lembrete({titulo, horario, data}){
+export default function Lembrete({id,titulo, horario, data}){
+
+    const formatoData = (data) => {
+        let dataN = data.split(" ")[0]
+        dataN = dataN.split('-')[2]+'/'+dataN.split('-')[1]+'/'+dataN.split('-')[0]
+        return dataN
+    }
+
+    const formatoHora = (hora) => {
+        let horaN = hora.split(" ")[1]
+        return horaN
+    }
+
+    
+
     if(data == null){
     return(
         <NativeBaseProvider>
             <Box style={[styleLemb.card]}>
                 <Text style={[styleLemb.titulo]}>{titulo}</Text>
                 <Box style={[styleLemb.miniCard]}>
+
                     <Box flexDirection={'row'}>
                         <IconIO name='time-outline' size={25} style={[styleLemb.icon]}/>
-                        <Text style={styleLemb.horarioTexto}>{horario}</Text>
+                        <Text style={styleLemb.horarioTexto}>{formatoHora(horario)}</Text>
                     </Box>
                     
                 </Box>
