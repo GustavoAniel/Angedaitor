1f12f710f3142d0d66611ed2a1a290bd4cfe4b80
Gustavo <aniel.amtimil@yahoo.com.br>
2022-05-19 00:40:07 -0300

    Adicionado validações de data

--- src/components/Lembretes.js
+++ src/components/Lembretes.js
@@ -5,11 +5,30 @@ import styleLemb from '../styles/Lembrete'
 import IconIO from 'react-native-vector-icons/Ionicons'
 import IconFeather from 'react-native-vector-icons/Feather'
 
-export default function Lembrete({id,titulo, horario, data}){
+export default function Lembrete({id,titulo, horario, data, finalizado}){
+    const dataAgora = () => {
+        let date = new Date();
+
+        let dia = date.getDate();
+        let mes = date.getMonth()+1;
+
+        if(dia < 10){
+            dia = '0'+dia;
+        }
+
+        if(mes < 10){
+            mes = '0'+mes;
+        }
+
+        let result = dia+'/'+mes+'/'+date.getFullYear()
+
+        return result
+    }
 
     const formatoData = (data) => {
         let dataN = data.split(" ")[0]
         dataN = dataN.split('-')[2]+'/'+dataN.split('-')[1]+'/'+dataN.split('-')[0]
+
         return dataN
     }
 
@@ -18,37 +37,78 @@ export default function Lembrete({id,titulo, horario, data}){
         return horaN
     }
 
+    if(finalizado != 1){
     
+        if(formatoData(data) == dataAgora()){
+        return(
+            <NativeBaseProvider>
+                <Box style={[styleLemb.card]}>
+                    <Text style={[styleLemb.titulo]}>{titulo}</Text>
+                    <Box style={[styleLemb.miniCardAzul]}>
 
-    if(data == null){
-    return(
-        <NativeBaseProvider>
-            <Box style={[styleLemb.card]}>
-                <Text style={[styleLemb.titulo]}>{titulo}</Text>
-                <Box style={[styleLemb.miniCard]}>
-
-                    <Box flexDirection={'row'}>
-                        <IconIO name='time-outline' size={25} style={[styleLemb.icon]}/>
-                        <Text style={styleLemb.horarioTexto}>{formatoHora(horario)}</Text>
+                        <Box flexDirection={'row'}>
+                            <IconIO name='time-outline' size={25} style={[styleLemb.icon]}/>
+                            <Text style={styleLemb.horarioTexto}>{formatoHora(horario)}</Text>
+                        </Box>
+                        
+                        <Box flexDirection={'row'}>
+                                <IconFeather name='calendar' size={25} style={[styleLemb.icon]}/>
+                                <Text style={styleLemb.horarioTexto}>Hoje</Text>
+                        </Box>
                     </Box>
-                    
                 </Box>
-            </Box>
-        </NativeBaseProvider>
-    )
+            </NativeBaseProvider>
+        )
+        }else if(formatoData(data) > dataAgora()){
+            return(
+                <NativeBaseProvider>
+                    <Box style={[styleLemb.card]}>
+                        <Text style={[styleLemb.titulo]}>{titulo}</Text>
+                        <Box style={[styleLemb.miniCardCinza]}>
+                            <Box flexDirection={'row'}>
+                                <IconIO name='time-outline' size={25} style={[styleLemb.icon]}/>
+                                <Text style={styleLemb.horarioTexto}>{formatoHora(horario)}</Text>
+                            </Box>
+                            <Box flexDirection={'row'}>
+                                <IconFeather name='calendar' size={25} style={[styleLemb.icon]}/>
+                                <Text style={styleLemb.horarioTexto}>{formatoData(data)}</Text>
+                            </Box>
+                        </Box>
+                    </Box>
+                </NativeBaseProvider>
+            )
+        }else{
+            return(
+                <NativeBaseProvider>
+                    <Box style={[styleLemb.card]}>
+                        <Text style={[styleLemb.titulo]}>{titulo}</Text>
+                        <Box style={[styleLemb.miniCardVermelho]}>
+                            <Box flexDirection={'row'}>
+                                <IconIO name='time-outline' size={25} style={[styleLemb.icon]}/>
+                                <Text style={styleLemb.horarioTexto}>{formatoHora(horario)}</Text>
+                            </Box>
+                            <Box flexDirection={'row'}>
+                                <IconFeather name='calendar' size={25} style={[styleLemb.icon]}/>
+                                <Text style={styleLemb.horarioTexto}>{formatoData(data)}</Text>
+                            </Box>
+                        </Box>
+                    </Box>
+                </NativeBaseProvider>
+            )
+        }
     }else{
         return(
             <NativeBaseProvider>
                 <Box style={[styleLemb.card]}>
                     <Text style={[styleLemb.titulo]}>{titulo}</Text>
-                    <Box style={[styleLemb.miniCard]}>
+                    <Box style={[styleLemb.miniCardVerde]}>
                         <Box flexDirection={'row'}>
                             <IconIO name='time-outline' size={25} style={[styleLemb.icon]}/>
-                            <Text style={styleLemb.horarioTexto}>{horario}</Text>
+                            <Text style={styleLemb.horarioTexto}>{formatoHora(horario)}</Text>
                         </Box>
                         <Box flexDirection={'row'}>
                             <IconFeather name='calendar' size={25} style={[styleLemb.icon]}/>
-                            <Text style={styleLemb.horarioTexto}>{data}</Text>
+                            <Text style={styleLemb.horarioTexto}>{formatoData(data)}</Text>
                         </Box>
                     </Box>
                 </Box>
